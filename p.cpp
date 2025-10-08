4d75c7fa6b151f32b2134140be998e61d67dee88
ADMIN <michaelndegwa997@gmail.com>
2025-10-08 14:36:37 +0300

    C++ Nested If

--- p.cpp
+++ p.cpp
@@ -5,10 +5,16 @@ using namespace std;
 int main() {
 int myAge;
 int votingAge = 18;
+bool isCitizen = true;
 cout<< "Input you age:";
 cin >> myAge;
 if (myAge >= votingAge) {
-  cout << "Old enough to vote!";
+  cout << "Old enough to vote! \n";
+  if (isCitizen) {
+    cout << "And you are a citizen, so you can vote!\n";
+  } else {
+    cout << "But you must be a citizen to vote.\n";
+  }
 } else {
   cout << "Not old enough to vote.";
 }

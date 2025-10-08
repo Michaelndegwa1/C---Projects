25b104877ff8596d1b8920345ddd2c584d631451
ADMIN <michaelndegwa997@gmail.com>
2025-10-08 13:15:30 +0300

    endl manipulator

--- p.cpp
+++ p.cpp
@@ -5,6 +5,8 @@ int main() {
   cout << "Hello World! \n";//
   cout << "I am learning C++ in ";
   cout << 3+2;
-  cout << " Months";
+  cout << " Months" <<endl;
+  cout << "I am very happy ";
+
   return 0;
 } 

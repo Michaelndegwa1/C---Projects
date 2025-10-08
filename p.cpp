00f0ba3c060bd47b1160c6cb5d871748bfdb7e44
ADMIN <michaelndegwa997@gmail.com>
2025-10-08 13:32:54 +0300

    Creating a Simple Calculator

--- p.cpp
+++ p.cpp
@@ -4,24 +4,12 @@ to the screen, and it is amazing */
 using namespace std;
 
 int main() {
-  cout << "Hello World! \n";// This is a comment
-  cout << "I am learning C++ in ";
-  cout << 3+2; 
-  cout << " Months" <<endl;
-  cout << "I am very happy for \n ";
- 
-int myNumb = 5;               // Integer (whole number without decimals)
-double myFloatNum = 5.99;    // Floating point number (with decimals)
-char myLetter = 'D';         // Character
-string myText = " Days";     // String (text)
-bool myBoolean = true; 
-cout << myFloatNum;
-cout << myText;
-
-//Declare Many Variables
-int x = 5, y = 6, z = 50;
-cout << x + y + z;
-  return 0;
-
-
+ int x, y;
+int sum;
+cout << "Type a number: ";
+cin >> x;
+cout << "Type another number: ";
+cin >> y;
+sum = x + y;
+cout << "Sum is: " << sum; 
 } 

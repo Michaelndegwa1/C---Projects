6e9de3c45fec8d1d1fa0892fcdd17ca8dee019de
ADMIN <michaelndegwa997@gmail.com>
2025-10-08 13:17:06 +0300

    C++ Comments

--- p.cpp
+++ p.cpp
@@ -1,10 +1,12 @@
+/* The code below will print the words Hello World and time i will take to learn C++!
+to the screen, and it is amazing */
 #include <iostream>
 using namespace std;
 
 int main() {
-  cout << "Hello World! \n";//
+  cout << "Hello World! \n";// This is a comment
   cout << "I am learning C++ in ";
-  cout << 3+2;
+  cout << 3+2; 
   cout << " Months" <<endl;
   cout << "I am very happy ";
 
